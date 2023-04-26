9c90ec112f1e179d656513fc432e83a83996535b
Carolina Louzada <carolina.louzada@venturus.org.br>
2023-04-26 11:28:03 -0300

    Atualizando test cases para aula 2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,49 +1,6 @@
 describe('Aprendendo conceitos Cypress', () => {
 
-    it('Acessando home da página Automation Exercise', () => {
-    cy.visit('/');
-    cy.contains('Automation');
-    cy.get('h1'); // acessando tag 
-    cy.get('h1').contains('Automation'); 
-
-    cy.get('.features_items');// verificar se a seção 'features items' existe na página inicial a partir da classe
-    //or
-    cy.get('div.features_items');//tag + classe
-
-  })
-  it('Acessando home da página Automation Exercise', () => {
-    cy.visit('/');
-    cy.contains('Automation');
-    cy.get('h1'); // acessando tag 
-    cy.get('h1').contains('Automation'); 
-
-    cy.get('.features_items');// verificar se a seção 'features items' existe na página inicial a partir da classe
-    //or
-    cy.get('div.features_items');//tag + classe
-
-  })
-
-  it('Verificando itens para compra', () => {
-    cy.visit('/');
-    cy.get('.features_items'); // acessando prieiro elemento filho do item
-    cy.get('.features_items').children().first();
-    cy.get('.features_items').children().last(); // acessando filhos de um elemento
-    cy.get('.features_items').children().eq(2); // acessando elemento de array
-
-    cy.get('[data-product-id="2"]') // pelo data-id
-
-  })
-
-  it('Colocar item no carrinho e continuar comprando', () => {
-    cy.visit('/');
-    cy.get('[data-product-id="2"]').contains('Add to cart').click();
- 
-    cy.get('#cartModal').contains('Added');
-    cy.contains('button.close-modal').click();
-
-  })
-
-  it.only('Usuario faz login com username e senha inválidos', () => {
+  it('1 - Usuário faz login com username e senha inválidos', () => {
     cy.visit('/');
     cy.get('div.shop-menu').contains('Login').click();
 
@@ -58,7 +15,57 @@ describe('Aprendendo conceitos Cypress', () => {
     cy.get('[data-qa="login-button"]').contains('Login').click();
 
     cy.contains('Your email or password is incorrect!');
+  })
+    it('2 - Acessando home da página Automation Exercise', () => {
+    cy.visit('/');
+    cy.contains('Automation');
+    cy.get('h1'); // acessando tag 
+    cy.get('h1').contains('Automation'); 
+
+    cy.get('.features_items');// verificar se a seção 'features items' existe na página inicial a partir da classe
+    //or
+    cy.get('div.features_items');//tag + classe
+
+  });
+
+  it('3 - Verificando itens para compra', () => {
+    cy.visit('/');
+    cy.get('.features_items'); // acessando prieiro elemento filho do item
+    cy.get('.features_items').children().first();
+    cy.get('.features_items').children().last(); // acessando filhos de um elemento
+    cy.get('.features_items').children().eq(2); // acessando elemento de array
+
+    cy.get('[data-product-id="2"]') // pelo data-id
 
   })
-})
+
+  it('4 - Colocar item no carrinho e continuar comprando', () => {
+    cy.visit('/');
+    cy.get('[data-product-id="2"]').contains('Add to cart').click();
+ 
+    cy.get('#cartModal').contains('Added');
+
+    cy.get('button.close-modal', {timeout: 5000}).click();
+
+  });
+
+  it('5 - Acessando página de produtos - usando intercept', () => {
+    cy.visit('/');
+    cy.intercept('GET','products' );
+    cy.get('.navbar-nav').contains('Products').click();
+    //cy.get('a[href^="/products"]').contains('Products').click();
+  });
+
+  it('6 - GET Produtos - usando request', () => {
+   
+    cy.request('GET', 'api/productsList');
+
+  });
+});
+
+cy.get(':checkbox').should('be.disabled')
+
+cy.get('form').should('have.class', 'form-horizontal')
+
+cy.get('input').should('not.have.value', 'US')
 
